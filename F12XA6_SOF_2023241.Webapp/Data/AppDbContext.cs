8a326a9015da954db98541626f7e782dedcbc00d
KenyeresGergo <keny.gergo@gmail.com>
2023-10-27 18:02:04 +0200

    AppDbCntext added

---
+++ F12XA6_SOF_2023241.Webapp/Data/AppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using F12XA6_SOF_2023241.Webapp.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace F12XA6_SOF_2023241.Webapp.Data
+{
+    public class AppDbContext : IdentityDbContext
+    {
+       public DbSet<Game> Games { get; set; }
+
+        public DbSet<AppUser> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Game>()
+                .HasOne(t => t.Owner)
+                .WithMany()
+                .HasForeignKey(t => t.OwnerId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            base.OnModelCreating(builder);
+        }
+    }
+}
