de915ad908e6353715b435bc9639249c8fc500f1
KenyeresGergo <keny.gergo@gmail.com>
2023-11-14 13:55:51 +0100

    CRUD methods added to gmaeLogic

--- F12XA6_SOF_2023241.Logic/GameLogic.cs
+++ F12XA6_SOF_2023241.Logic/GameLogic.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.Mvc;
 
 namespace F12XA6_SOF_2023241.Logic
 {
@@ -15,10 +16,28 @@ namespace F12XA6_SOF_2023241.Logic
         {
             this.context = context;
         }
+
+        public void Create(Game game)
+        {
+            var gameId = context.Games.FirstOrDefault(x => x.Id == game.Id);
+            if (gameId != null)
+            {
+                throw new ArgumentException("Game with thos name already exists!");
+            }
+           context.Games.Add(game);
+           context.SaveChanges();
+        }
         public IEnumerable<Game> Read()
         {
             return this.context.Games;
         }
+        public void Update()
+        {   
         
+        }
+        public void Delete() 
+        {
+        
+        }
     }
 }
