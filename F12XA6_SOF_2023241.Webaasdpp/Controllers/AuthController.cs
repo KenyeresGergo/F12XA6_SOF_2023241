7e9c104629c18ba1f2b451c5929cb769c4b3549c
KenyeresGergo <keny.gergo@gmail.com>
2023-11-15 14:31:24 +0100

    Webapp with identity added

---
+++ F12XA6_SOF_2023241.Webaasdpp/Controllers/AuthController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace F12XA6_SOF_2023241.Webapp.Controllers
+{
+    public class AuthController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- F12XA6_SOF_2023241.Webaasdpp/Program.cs
+++ F12XA6_SOF_2023241.Webaasdpp/Program.cs
@@ -1,5 +1,8 @@
-using F12XA6_SOF_2023241.Webapp.Data;
+using F12XA6_SOF_2023241.Repository;
+using F12XA6_SOF_2023241.Logic;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.EntityFrameworkCore;
 
 namespace F12XA6_SOF_2023241.Webapp
@@ -9,25 +12,31 @@ namespace F12XA6_SOF_2023241.Webapp
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+              .AddEntityFrameworkStores<AppDbContext>()
+              .AddDefaultTokenProviders();
+            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+            builder.Services.AddDbContext<AppDbContext>(options =>
+                options
+                    .UseSqlServer(connectionString)
+                    .UseLazyLoadingProxies()
+            );
+
+
+            //builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
             // Add services to the container.
-            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-            builder.Services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(connectionString));
-            builder.Services.AddDatabaseDeveloperPageExceptionFilter();
-
-            builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-                .AddEntityFrameworkStores<ApplicationDbContext>();
             builder.Services.AddControllersWithViews();
+
+
+            //builder.Services.AddScoped<GameLogic>();
+            //builder.Services.AddScoped<F12XA6_SOF_2023241.Logic.GameLogic>();
 
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseMigrationsEndPoint();
-            }
-            else
+            if (!app.Environment.IsDevelopment())
             {
                 app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
@@ -39,14 +48,13 @@ namespace F12XA6_SOF_2023241.Webapp
 
             app.UseRouting();
 
-            app.UseAuthentication();
             app.UseAuthorization();
+            app.UseAuthentication();
 
             app.MapControllerRoute(
                 name: "default",
                 pattern: "{controller=Home}/{action=Index}/{id?}");
-            app.MapRazorPages();
-
+            //app.MapRazorPages();
             app.Run();
         }
     }
