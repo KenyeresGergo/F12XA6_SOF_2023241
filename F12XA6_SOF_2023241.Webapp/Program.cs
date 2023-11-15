00c8d8db749c41397c8a3bf8f84f80f1181bafd2
dnjoel <dnjoel52@gmail.com>
2023-11-15 15:28:29 +0100

    navbar fix

--- F12XA6_SOF_2023241.Webapp/Program.cs
+++ F12XA6_SOF_2023241.Webapp/Program.cs
@@ -1,4 +1,4 @@
-using F12XA6_SOF_2023241.Webapp.Data;
+using F12XA6_SOF_2023241.Repository;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
@@ -12,12 +12,12 @@ namespace F12XA6_SOF_2023241.Webapp
 
             // Add services to the container.
             var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+            builder.Services.AddDbContext<AppDbContext>(options =>
                 options.UseSqlServer(connectionString));
             builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
             builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-                .AddEntityFrameworkStores<ApplicationDbContext>();
+                .AddEntityFrameworkStores<AppDbContext>();
             builder.Services.AddControllersWithViews();
 
             var app = builder.Build();
