b10eaebe40f855a71e254b27c4f9e11dc9b031e3
KenyeresGergo <keny.gergo@gmail.com>
2023-11-21 14:52:33 +0100

    interface fiix

--- F12XA6_SOF_2023241.Logic/Interfaces/IGameLogic.cs
+++ F12XA6_SOF_2023241.Logic/Interfaces/IGameLogic.cs
@@ -6,6 +6,7 @@ namespace F12XA6_SOF_2023241.Logic.Interfaces
     {
         void Create(Game game);
         void Delete(string id);
+        IEnumerable<Game> GamesByStudioId(string studioid);
         IEnumerable<IEnumerable<Game>> GamesByStudios();
         IEnumerable<Game> MyGames(AppUser user);
         IEnumerable<Game> Read();
