6d5394c5f7ba2cf039b309824a2247c8df8b6fdd
KenyeresGergo <keny.gergo@gmail.com>
2024-01-29 18:02:27 +0100

    comment logic implemented

---
+++ F12XA6_SOF_2023241.Logic/CommentLogic.cs
@@ -0,0 +1,22 @@
+﻿using F12XA6_SOF_2023241.Models;
+using F12XA6_SOF_2023241.Repository.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace F12XA6_SOF_2023241.Logic
+{
+    public class CommentLogic
+    {
+        private IRepository<Comment> repository;
+        public CommentLogic(IRepository<Comment> repository)
+        {
+            this.repository = repository;
+        }
+
+
+
+    }
+}
